14464484cce92f93e9c6041009522d8565db1d96
Alan Dodge <alphaechododge@gmail.com>
2019-09-24 17:00:46 +0000

    Changed typewiter to take a delay variable

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,14 @@
 const sentence = "hello i love you wount you tell me your name";
 
-const printString = function(string) {
+const printString = function(string, delay) {
   if (string[0]) {
     setTimeout(() => {
       process.stdout.write(string[0]);
-      printString(string.slice(1));
-    }, 50);
+      printString(string.slice(1), delay);
+    }, delay);
   } else {
     console.log();
   }
 };
 
-printString(sentence);
+printString(sentence, 50);
